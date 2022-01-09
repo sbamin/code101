0667622789c9b95dac1207bc26d6dfd22f0bd824
Samir B Amin <sbamin@alumni.bcm.edu>
2022-01-09 16:23:09 -0500

    published commit: 91879a7 and mkdocs 1.2.3

--- 1.1/assets/js/katex.js
+++
@@ -1,9 +0,0 @@
-// Credit: @walkcc, https://github.com/walkccc/CS
-document.addEventListener("DOMContentLoaded", function() {
-    renderMathInElement(document.body, {
-        delimiters: [
-            {left: "$$", right: "$$", display: true},
-            {left: "$", right: "$", display: false}
-        ]
-    });
-});
